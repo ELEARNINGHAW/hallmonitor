502e8ec6866b3d57ef7dc78899d78e3f67e29add
Werner <werner.welte@gmx.de>
2023-07-20 10:37:25 +0200

    V1.3 add:Raumfinder

---
+++ klausshowW22.php
@@ -0,0 +1,166 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/jquery.dm-uploader.min.css" rel="stylesheet">
+    <link href="css/upload-styles.css" rel="stylesheet">
+  <script src = "js/jquery-1.10.2.min.js"></script>
+  <script src = "js/jquery-ui-1.13.2/jquery-ui.min.js"></script>
+  <link  href = "js/jquery-ui-1.13.2/jquery-ui.min.css" rel="stylesheet" >
+  <script src="js/mousemove.js"></script>
+<link rel="stylesheet"  type="text/css" href="css/circle-menu.min.css">
+<style>
+ h1, h2, h3, tr {  text-align:center;padding: 0px;  -moz-user-select:none; cursor:pointer; }
+ .ui-accordion-header:hover{ color:#000000; background-color: #DDDDDD; }
+ .ui-accordion-header:active{ color:#ffffff; background-color: #003399; }
+
+#hover1, #hover2
+{
+ width:100%;
+ height:60px;
+ border:1px solid #cc0000;   
+ background-color:#ACACAC;
+ text-align: center;
+ top:-20px;
+}
+
+
+#container
+{
+  width:100%;
+  height:960px;
+  overflow-y:auto;   
+  border:1px solid #000;
+  overflow: hidden;
+}
+
+.udnav
+{
+  width:75px;
+  height:60px;
+}
+
+
+</style>
+
+<script>
+function toggleKl( kid) 
+{ var x = document.getElementById(kid);
+  if (x.style.display == "block"){
+      x.style.display = "none";
+   } else {
+      x.style.display = "block";
+   }
+}
+
+</script>
+</head>
+
+<body id="buehne" onload="startTimeOutTimer();" style="padding-bottom:0px; padding-top: 0px;">
+<div id="hover1"> <img class="udnav" src="img/up1.png"/> </div>
+<div id="container">
+<h1 style="background-color: black; color:white; padding: 20px; ">Bewertungen WS 2022 </h1>
+<?php
+ $semester= '22W';
+ $i = 0;
+ $db    =  new SQLite3('../db/klausurnotenW.db' );
+ $SQL = 'SELECT * FROM "' . $semester . '" ';	
+print_r($SQL);
+ $ret   = $db -> query( $SQL );
+
+ while ( $row = $ret -> fetchArray( ) )
+ { $klausuren[ $row[ 'Prüfende(r)' ] ][  $row[ 'Prüfung' ]  ][  $row[ 'Matnr3' ]  ] = $row;
+ }
+
+ foreach ($klausuren as $kk => $kv)
+  { $lecturer = explode('.',$kk  );
+	
+	if (isset( $lecturer[ 1 ] ) )         { $sname = trim( $lecturer[ 1 ] ). ' '. trim( $lecturer[ 0 ] ).'.' ; }
+    else if( trim($lecturer[ 0 ]) != '' ) { $sname = trim( $lecturer[ 0 ] ) ;}
+  
+    $lec[ $sname ][ 'sname' ] = $sname;
+    $lec[ $sname ][ 'oname' ] = $kk;
+  }
+  ksort($lec);
+  echo '<div id="klaus">'; 
+  foreach($lec as $l)
+  { #print_r($klausuren); die();
+     $i++;
+     $id = 'klaus'.$i;
+	 echo '<a onclick="toggleKl(\''.$id.'\')"><h1>' .$l['sname']. ' ['. $i.'] </h1></a>'; 
+	 echo '<div id = "'.$id.'" style="border: solid black 2px; display: none;">';
+    # echo '<h1>' .$l['sname']. ' ['. $i.'] </h1>'; 
+	# echo '<div id = "'.$id.'" style="border: solid 4px blue;">';
+		
+	
+	 ksort ($klausuren[ $l['oname']]);
+	 
+	 foreach ($klausuren[ $l['oname']] as $pk => $prüfung)
+	 { ksort($prüfung);
+	 
+	   echo "<a><h2>".$pk.'</h2></a>'; 
+		  
+       $kl = "<table>";
+	   foreach ($prüfung as  $p ) { $kl .= '<tr><td style="width:48%">&nbsp;</td><td>'.$p['Matnr3'].'</td><td style="width:50px">&nbsp;</td><td>'.$p['Bewertung1Dez'].'</td><td style="width:48%">&nbsp;</td></tr> ';  }
+       $kl .=  "</table>";	
+	   echo $kl;
+	 }
+
+	echo "</div>";
+  }
+ echo '</div>'; 
+ echo '</div>'; 
+ 
+
+ 
+
+?>
+ <div id="hover2">  <img class="udnav" src="img/down1.png"/> </div>
+<script>
+
+var amount = '';
+
+function scroll() {
+	      console.log(amount);
+    $('#container').animate({ 
+        scrollTop: amount
+    }, 100, 'linear',function() {
+        if (amount != '') {
+            scroll();
+        }
+    });
+}
+$('#hover1').hover(function() {
+    amount = '+=100';
+    scroll();
+}, function() {
+    amount = '';
+});
+$('#hover2').hover(function() {
+    amount = '-=100';
+    scroll();
+}, function() {
+    amount = '';
+});
+
+
+
+ 	
+
+$( "#klaus" ).accordion({
+ heightStyle: "content",
+   collapsible: true,
+ active: false ,
+});
+</script>
+
+<nav class="c-circle-menu js-menu">
+  <button class="c-circle-menu__toggle is-active js-menu-toggle">
+    <a href="index.php"><span>Toggle</span></a>
+  </button>
+</nav>
+
+  </body>
+</html>
---
+++ upload-xls-1.php
@@ -0,0 +1,146 @@
+<?php
+session_start();
+if( $_SESSION[ 'user_email' ] == "" )
+{ header( 'location:login/index.php' );
+}
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/jquery.dm-uploader.min.css" rel="stylesheet">
+    <link href="css/upload-styles.css" rel="stylesheet">
+     <link href="css/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <script src="js/jquery-1.10.2.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/dist/jquery.dm-uploader.min.js"></script>
+    <script src="js/upload-ui.js"></script>
+    <script src="js/upload-xls-config.js"></script>
+<?php
+
+#try { if ( !isset($_FILES['file']['error']) ||   is_array($_FILES['file']['error']) )
+#	{ throw new RuntimeException('Invalid parameters.');    }
+/*
+    switch ( $_FILES[ 'file' ][ 'error' ] ) 
+	{   case UPLOAD_ERR_OK:             break;
+        case UPLOAD_ERR_NO_FILE:        throw new RuntimeException('No file sent.');
+        case UPLOAD_ERR_INI_SIZE:     
+       		case UPLOAD_ERR_FORM_SIZE:  throw new RuntimeException('Exceeded filesize limit.');
+            default:                    throw new RuntimeException('Unknown errors.');
+    }
+ */
+
+if (isset($_FILES[ 'file' ]))
+{	
+  $file[ 'name' ] =  $_FILES[ 'file' ][ 'name' ];
+  $file[ 'dir'  ] = 'backend/files/';
+  $file[ 'path' ] = [ 'dir'  ].$file[ 'name' ]  ;
+
+  if (!is_writable($file[ 'dir' ])){ die( 'Keine Schreibrechte im Verzeichnis ' .$file[ 'dir'  ]); }
+
+  if ( !move_uploaded_file( $_FILES['file']['tmp_name'], $file[ 'path' ] ))
+  { throw new RuntimeException('Failed to move uploaded file.');
+  }
+
+    // All good, send the response
+    echo json_encode([
+        'status' => 'ok',
+        'path'   => $file[ 'path' ]
+    ]);
+ 
+   require('spreadsheet-reader/php-excel-reader/excel_reader2.php');
+   require('spreadsheet-reader/SpreadsheetReader.php');
+
+ $i = 0;
+ $headlineNr = 4;   # In welcher XLS Zeile sind die Bezeichner der Spalten
+ $semester = '22W'; # Semestername = Tabellenname
+ 
+ $heads[ 0 ][ 'xslName' ] = "Prüfender(in)"  ;
+ $heads[ 1 ][ 'xslName' ] = "Prüfung"	     ;
+ $heads[ 2 ][ 'xslName' ] = "Prüfungsnr."	 ;
+ $heads[ 3 ][ 'xslName' ] = "matrikelnr"	 ;
+ $heads[ 4 ][ 'xslName' ] = "bewertung"      ;
+ $heads[ 0 ][ 'dbName'  ] = "Prüfende(r)"    ;
+ $heads[ 1 ][ 'dbName'  ] = "Prüfung"        ;
+ $heads[ 2 ][ 'dbName'  ] = "Prüfungsnr"     ;
+ $heads[ 3 ][ 'dbName'  ] = "Matnr3"         ;
+ $heads[ 4 ][ 'dbName'  ] = "Bewertung1Dez"  ;
+
+ $db    =  new SQLite3('../db/klausurnotenW.db' );
+ 
+ if (isset($_GET['File']))
+ {  $file[ 'path' ] = $_GET['File'];
+ }
+
+ try
+ { $SQL = 'DELETE FROM "' . $semester.'"'; 
+   $ret   = $db -> query( $SQL );
+ # deb($ret,1);
+   $Reader = new SpreadsheetReader($file[ 'path' ]);
+   foreach ($Reader as $Row)
+   { ++$i; 
+     if ( $i == $headlineNr )
+	 { foreach( $Row as $rk=> $rv )
+	   { foreach( $heads as $hk => $hv )
+		 {  if ( $hv['xslName'] == $rv )  { $heads[$hk]['colNr'] = $rk; }
+		 }
+	   }
+	 }	
+     elseif ($i > $headlineNr)
+	 { $varia = $value = '';
+	   foreach( $heads as $hk => $hv ) 
+	   { $val0 = $Row[ $hv['colNr'] ];
+		 if      ( $heads[  $hk ][ 'xslName' ] == 'matrikelnr' ) 
+ 		 { $val = substr($val0 , -3) ; }
+ 
+         else if ( $heads[  $hk ][ 'xslName' ] == 'bewertung' )   
+	    { if (ctype_digit(trim($val0)))  { $val0= $val0.'_'; $val = $val0[0].'.'.$val0[1]; }
+       	  else                           { $val = $val0; } 
+		}
+		else                             { $val = $val0; }
+        
+        $varia .= '"'. $hv['dbName'] .'",';
+	    $value .= '"'. $val .'",';
+	   }
+   
+		$varia = rtrim($varia, "," );
+		$value = rtrim($value, "," );
+		
+		$SQL = 'INSERT INTO "' . $semester . '" ( ' .$varia. ' ) VALUES( '. $value. ' )';	
+		echo "\n".$SQL;
+		$ret   = $db -> query( $SQL );
+      }	 
+	} unlink ($file[ 'path' ]); 
+   }
+  catch (Exception $E)
+  { echo $E -> getMessage();
+  }
+}
+else 
+{
+?>
+ <div id="drag-and-drop-zone-1" class="dm-uploader p-5">
+   <h3 class="mb-5 mt-5 text-muted">EXCEL W22</h3>
+   <div class="btn btn-primary btn-block mb-5">
+     <span>open</span>
+     <input type="file"  title='Click to add Files' />
+     
+   </div>
+ </div> 
+<?php	
+}
+?>
+ 
+<div id="output" style ="width:100%; heigth:50px; background-color:#CCCCCC;" >123</div>
+
+
+<div  style ="float: right; margin-right: 50px;" ><button class="ssnew" ><a href="login/logout.php">Logout</a></button></div>
+<div  style ="float: right; margin-right: 50px;" ><button class="ssnew" ><a href="editor.php">EDITOR</a></button></div>
+
+  </body>
+</html>
